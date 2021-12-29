8c58246ba6f4cc316986f939cc07afc09dbe5226
“Nathalie <nathalielauture1@gmail.com>
2021-12-29 12:53:49 -0500

    update TenantLease

--- client/src/component/TenantLease.js
+++ client/src/component/TenantLease.js
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import { useParams } from "react-router";
 import { CurrentUserContext } from "./CurrentUserContext";
+import { MessageBox } from "./MessageBox";
 
 export const initialStateTenantLease = {
   _id: "",
@@ -13,7 +14,7 @@ export const initialStateTenantLease = {
   inclusion: "",
   rule: "",
   price: "",
-  idLodging:"",
+  idLodging: "",
   idUser: "",
 };
 
@@ -22,9 +23,11 @@ export const TenantLease = () => {
 
   const [formData, setFormData] = useState(initialStateTenantLease);
   const [lease, setLease] = useState(null);
-  const [tenant,setTenant]=useState(null)
+  const [tenant, setTenant] = useState(null);
+  const [name, setName] = useState(null);
   const param = useParams();
   const _id = param.tenantId;
+  const [message, setMessage] = useState("");
   //console.log("id:", _id);
 
   const history = useHistory();
@@ -36,17 +39,16 @@ export const TenantLease = () => {
       .then((data) => {
         if (data.data) {
           setTenant(data.data);
+          setName(data.data.firstName+" "+data.data.lastName)
           fetch(`/api/lease?idUser=${_id}`)
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.data) {
-              setLease(data.data);
-            }
-          });
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.data) {
+                setLease(data.data);
+              }
+            });
         }
       });
-
-     
   }, []);
 
   const handleChange = (value, name) => {
@@ -56,8 +58,8 @@ export const TenantLease = () => {
 
   const handleClick = () => {
     formData._id = uuidv4();
-   formData.idUser= tenant._id;
-  
+    formData.idUser = tenant._id;
+
     formData.idOwner = currentUser._id;
     fetch("/api/leases", {
       method: "POST",
@@ -71,6 +73,11 @@ export const TenantLease = () => {
       .then((json) => {
         const { status, data, message } = json;
         console.log(status, data, message);
+        setMessage("Lease added - wait until it saved please");
+        setTimeout(() => {
+          window.location.reload();
+          }, 2000);     
+        
       });
   };
 
@@ -79,7 +86,7 @@ export const TenantLease = () => {
       email: tenant.email,
       message: `<br/><Div>Date start: ${lease.dateStart}</Div><br/><Div>Date end: ${lease.dateEnd}</Div></br><Div>Inclusion: ${lease.inclusion}</Div></br><Div>Rule: ${lease.rule}</Div><Div>Price: ${lease.price}</Div>",
       subject: "Lease confirmation`,
-      name: tenant.firstName+tenant.lastName,
+      name: tenant.firstName + tenant.lastName,
     };
 
     fetch("/mail", {
@@ -94,195 +101,256 @@ export const TenantLease = () => {
       .then((json) => {
         const { status, data, message } = json;
         console.log(status, data, message);
+        setMessage("Lease has been send by email");             
       });
   };
 
+  
+
   return (
     <Wrapper>
+      <Header>
+        {message && <MessageBox message={message} setMessage={setMessage} />}
+      </Header>
+      <DivTenant>
       <Container>
-        <InfoBox>
-          {!lease && (
-            <>
+       
+            {!lease && tenant && (
+              <>
+               <ColumnBoxes>
+          <ColumnBox>
+                <InfoBox>
+                <Data>
+                    <Span>Name:</Span>
+                  </Data>
+                  <Data>
+                    <Span>Date start:</Span>
+                  </Data>
+
+                  <Data>
+                    <Span>Date end:</Span>
+                  </Data>
+
+                  <Data>
+                    <Span>Inclusion:</Span>
+                  </Data>
+
+                  <Data>
+                    <Span>Rule:</Span>
+                  </Data>
+
+                  <Data>
+                    <Span>Price:</Span>
+                  </Data>
+
+                  <Data>
+                    <Span># Lodging:</Span>
+                  </Data>
+                </InfoBox>
+                <InfoBoxRight>
+                
+                <Data>
+                <Input
+                      type="text"
+                      name="dateStart"
+                      placeholder={name}                  
+                    />
+                  </Data>
+               
+                  <Data>
+                    <Input
+                      type="text"
+                      name="dateStart"
+                      placeholder="Date Start"
+                      onChange={(ev) =>
+                        handleChange(ev.target.value, "dateStart")
+                      }
+                    />
+                  </Data>
+
+                  <Data>
+                    <Input
+                      type="text"
+                      name="dateEnd"
+                      placeholder="Date end"
+                      onChange={(ev) =>
+                        handleChange(ev.target.value, "dateEnd")
+                      }
+                    />
+                  </Data>
+                  <Data>
+                    <Input
+                      type="text"
+                      name="inclusion"
+                      placeholder="Inclusion"
+                      onChange={(ev) =>
+                        handleChange(ev.target.value, "inclusion")
+                      }
+                    />
+                  </Data>
+
+                  <Data>
+                    <Input
+                      type="text"
+                      name="rule"
+                      placeholder="Rule"
+                      onChange={(ev) => handleChange(ev.target.value, "rule")}
+                    />
+                  </Data>
+                  <Data>
+                    <Input
+                      type="text"
+                      name="price"
+                      placeholder="Price"
+                      onChange={(ev) => handleChange(ev.target.value, "price")}
+                    />
+                  </Data>
+                  <Data>
+                    <Input
+                      type="text"
+                      name="idLodging"
+                      placeholder="idLodging"
+                      onChange={(ev) =>
+                        handleChange(ev.target.value, "idLodging")
+                      }
+                    />
+                  </Data>
+                </InfoBoxRight>
+                </ColumnBox>
+                 <ButtonBox>
+                
+                     <Button onClick={handleClick}>Confirm</Button>
+                  
+                 </ButtonBox>
+                 </ColumnBoxes>
+              </>
+                
+            )}
+           
+
+         
+            {lease && tenant && (
+              <>
+              <ColumnBoxes>
+               <ColumnBox>
+                <InfoBox>
+                  <Data>
+                    <Span>Date start:</Span>
+                    <Span>{lease.dateStart}</Span>
+                  </Data>
+
+                  <Data>
+                    <Span>Date end:</Span>
+                    <Span>{lease.dateEnd}</Span>
+                  </Data>
+                  <Data>
+                    <Span>Inclusion:</Span>
+                    <Span>{lease.inclusion}</Span>
+                  </Data>
+
+                  <Data>
+                    <Span>Rule:</Span>
+                    <Span>{lease.rule}</Span>
+                  </Data>
+                  <Data>
+                    <Span>Price:</Span>
+                    <Span>{lease.price}$</Span>
+                  </Data>
+                  <Data>
+                    <Span># Lodging:</Span>
+                    <Span>{lease.idLodging}</Span>
+                  </Data>
+               
+                </InfoBox>
+                </ColumnBox>
+          <ButtonBox>
+            <Buttons>
+              <Button onClick={handleClickSend}>Send</Button>              
+            </Buttons>
+          </ButtonBox>
+          </ColumnBoxes>
+              </>
+            )}
             
-
-             
-
-              <Data>
-                <Span>Date start:</Span>
-                <Input
-                  type="text"
-                  name="dateStart"
-                  placeholder="Date Start"
-                  onChange={(ev) => handleChange(ev.target.value, "dateStart")}
-                />
-              </Data>
-
-              <Data>
-                <Span>Date end:</Span>
-                <Input
-                  type="text"
-                  name="dateEnd"
-                  placeholder="Date end"
-                  onChange={(ev) => handleChange(ev.target.value, "dateEnd")}
-                />
-              </Data>
-              <Data>
-                <Span>Inclusion:</Span>
-                <Input
-                  type="text"
-                  name="inclusion"
-                  placeholder="Inclusion"
-                  onChange={(ev) => handleChange(ev.target.value, "inclusion")}
-                />
-              </Data>
-
-              <Data>
-                <Span>Rule:</Span>
-                <Input
-                  type="text"
-                  name="rule"
-                  placeholder="Rule"
-                  onChange={(ev) => handleChange(ev.target.value, "rule")}
-                />
-              </Data>
-              <Data>
-                <Span>Price:</Span>
-                <Input
-                  type="text"
-                  name="price"
-                  placeholder="Price"
-                  onChange={(ev) => handleChange(ev.target.value, "price")}
-                />
-              </Data>
-              <Data>
-                <Span># Lodging:</Span>
-                <Input
-                  type="text"
-                  name="idLodging"
-                  placeholder="idLodging"
-                  onChange={(ev) => handleChange(ev.target.value, "idLodging")}
-                />
-              </Data>
-              <Buttons>
-                <Button onClick={handleClick}>Confirm</Button>
-                <Button onClick={handleClickSend}>Send</Button>
-
-                <Button>Cancel</Button>
-              </Buttons>
-            </>
-          )}
-
-          {lease && (
-            <>
-              
-
-              <Data>
-                <Span>Date start:</Span>
-                <Span>{lease.dateStart}</Span>
-              </Data>
-
-              <Data>
-                <Span>Date end:</Span>
-                <Span>{lease.dateEnd}</Span>
-              </Data>
-              <Data>
-                <Span>Inclusion:</Span>
-                <Span>{lease.inclusion}</Span>
-              </Data>
-
-              <Data>
-                <Span>Rule:</Span>
-                <Span>{lease.rule}</Span>
-              </Data>
-              <Data>
-                <Span>Price:</Span>
-                <Span>{lease.price}</Span>
-              </Data>
-              <Data>
-                <Span># Lodging:</Span>
-                <Span>{lease.idLodging}</Span>
-              </Data>
-              <Buttons>
-                <Button onClick={handleClick}>Confirm</Button>
-                <Button onClick={handleClickSend}>Send</Button>
-                <Button onClick={handleClick}>Cancel</Button>
-              </Buttons>
-            </>
-          )}
-        </InfoBox>
+   
       </Container>
+      </DivTenant>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  height: 830px;
   margin-left: 250px;
-  width: 85%;
-  height: 800px;
+  margin-top: 80px;
   font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
     "Lucida Sans", Arial, sans-serif;
 `;
 
+const Header = styled.div`
+  background-color: #006bb6;
+  display: flex; 
+  align-items: center;
+  height: 5%;
+  position: fixed;
+  width: 100%;
+  z-index: 1;
+`;
+
 const Container = styled.div`
   display: flex;
-  width: 80%;
+  width: 55%;
   height: 80%;
   flex-direction: row;
   align-items: center;
   color: white;
   background-color: rgb(194, 201, 202);
   border-radius: 5px;
-  margin: 2%;
-  margin-left: 5%;
+  margin-top: 80px;
+  margin-left: 5%; 
 `;
 
-const ItemImage = styled.img`
-  height: 500px;
-  width: 500px;
-`;
-const ImageBox = styled.div`
-  margin-top: 10px;
-  width: 500px;
-
-  border: 3px solid;
-  height: 500px;
-  border: none;
-  border-radius: 40px;
-  overflow: hidden;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-left: 70px;
-  margin-right: 70px;
-  margin-bottom: 20px;
+const DivTenant = styled.div`
+  //margin-top: 100px;
+  height:100%;
+  border: 2px solid transparent;
 `;
 
 const InfoBox = styled.div`
-  height: 500px;
-  width: 700px;
+  height: 100%;
+  width: 300px;
+  margin-left: 50px;
   display: flex;
   flex-direction: column;
-  margin-top: 10%;
+  align-items: center;  
+`;
+const InfoBoxRight = styled.div`
+  height: 100%;
+  width: 500px;
   margin-left: 50px;
-  
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
 `;
 
 const Span = styled.span`
-  margin-right: "28px";
+  margin-right: "18px";
   font-size: 28px;
- 
-  
-
+  height:100%;
 `;
 
 const Data = styled.div`
   
   margin-left: "10px";
-  margin-right: "8px";
+  margin-right: "5px";
   font-size: 17px;
   width:100%;
   margin-bottom: 5%;
   font-size: 28px;
+  display: flex;
+  align-items: center;
+  height: 100%;
+ 
  
 `;
 const Input = styled.input`
@@ -292,15 +360,17 @@ const Input = styled.input`
   color: #464a5c;
   font-size: 15px;
   font-weight: 300;
-  height: 36px;
+  height: 100%;
   padding: 8px 12px 10px 12px;
-  width: 50%;
+  width: 100%;
+  
 `;
 
 const Buttons = styled.button`
   display: flex;
 
   flex-direction: row;
+  justify-content: center;
   margin-left: 50px;
   background-color: transparent;
   border: none;
@@ -313,8 +383,32 @@ const Button = styled.button`
   padding: 12px;
   width: 300px;
   border: none;
-  margin-top: 10px;
+  margin-top: 30px;
   font-size: 17px;
   font-weight: bold;
-  margin-left: 50px;
+  margin-left: 50px; 
+`;
+
+const ColumnBoxes = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  margin-right: 100px;
+
+`;
+
+const ColumnBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  height:450px;
+ 
+`;
+
+const ButtonBox = styled.div`
+width: 100%;
+display: flex:
+align-items: center;
+justify-content:center;
+
+
 `;
